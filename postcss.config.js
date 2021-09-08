da8d8c97a4bfdda36a16cf7c55a906b19bbf7711
Moldogazy Kabylbekov <moldogazy.kabylbekov.kk@gmail.com>
2021-09-08 14:48:06 +0600

    TailwindCSS done

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // plugins: [
+  //   require('postcss-import'),
+  //   require('tailwindcss'),
+  //   require('autoprefixer'),
+    
+  //   tailwindcss: {},
+  //   autoprefixer: {},
+  // ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
